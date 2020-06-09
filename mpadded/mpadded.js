5482a2da937163b2e2e3e4f41c200164abe69d4e
Neil Soiffer <soiffer@alum.mit.edu>
2020-06-08 22:02:57 -0700

    Added mpadded transform (requires measurement) along with tests in acid-tests

---
+++ mpadded/mpadded.js
@@ -0,0 +1,119 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode:nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 et sw=4 tw=80: */
+/*
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+import { _MathTransforms } from '../common/math-transforms.js'
+const MATHML_NS = 'http://www.w3.org/1998/Math/MathML';
+
+/**
+ * @param {HTMLElement} mpadded
+ * @returns {{width:number, height: number: depth: number}}
+ */
+function getDimensions(mpadded) {
+    // Create an mrow around the children of 'mpadded' and add a zero height/depth mspace to them.
+    // the y/top/bottom of the mspace is the baseline, so we can find height/depth of el
+    // undo the changes to the DOM and return the values
+    // Note: the mspace should not cause reflow, so the change/undo hopefully is somewhat efficient
+    const mrow = document.createElementNS(MATHML_NS, 'mrow');
+    mrow.appendChild( document.createElementNS(MATHML_NS, 'mspace') );
+    const cloneMpadded = mpadded.cloneNode(true);
+    for (let i = 0; i < cloneMpadded.children.length; i++) {
+        mrow.appendChild(cloneMpadded.children[i]);    // removed from clone and added to mrow
+    }
+    cloneMpadded.appendChild(mrow);
+    mpadded.parentElement.replaceChild(cloneMpadded, mpadded);      // should not be reflow
+
+    const mspaceRect = mrow.firstElementChild.getBoundingClientRect();
+    const mpaddedRect = mrow.getBoundingClientRect();
+
+    cloneMpadded.parentElement.replaceChild(mpadded, cloneMpadded);      // restore original structure; should not reflow
+    return {
+        width: mpaddedRect.width,
+        height: mspaceRect.y - mpaddedRect.top,
+        depth: mpaddedRect.bottom - mspaceRect.y
+    };
+}
+/**
+ * @param {HTMLElement} el
+ * @param {string} attr
+ * @param {'width'|'height'|'depth'} dimension
+ * @param {{width:number, height: number: depth: number}} dimensions
+ * @returns {boolean}
+ */
+function replacePseudoAttr(el, attr, dimension, dimensions) {
+    const attrValue = el.getAttribute(attr).toLowerCase();
+    if (attrValue.includes(dimension)) {
+        const floatVal = parseFloat(attrValue) * dimensions[dimension] / (attrValue.includes('%') ? 100.0 : 1.0);
+        el.setAttribute(attr, floatVal.toFixed(1) + 'px');
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @param {HTMLElement} el
+ * @param {attr} align
+ * @param {{width:number, height: number: depth: number}} dimensions
+ * @returns {boolean}       // true if handled
+ */
+function handleAttr(el, attr, dimensions) {
+    if (!el.hasAttribute(attr)) {
+        return false;
+    }
+
+    const attrValue = el.getAttribute(attr).toLowerCase();
+    if (attrValue.includes(attr)) {
+        const floatVal = parseFloat(attrValue); // only parses number part
+        // replace the name is basically a no-op -- add '%' is not present
+        el.setAttribute(attr, `${floatVal.toFixed(2) * (attrValue.includes('%') ? 1 : 100)}%`);
+        return;
+    }
+
+    if (replacePseudoAttr(el, attr, 'width', dimensions)) {
+        return true;
+    }
+    if (replacePseudoAttr(el, attr, 'height', dimensions)) {
+        return true;
+    }
+    if (replacePseudoAttr(el, attr, 'depth', dimensions)) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @param {HTMLElement} el 
+ */
+const transformMpadded = (el) => {
+    // if the attr value contains a pseudo-unit (width, height, depth),
+    // these are converted to pixels unless it mentions itself, and then it is just '%' (part of core)
+    const dimensions = getDimensions(el);       // do this before changing the attr values
+
+    handleAttr(el, 'width', dimensions);
+    handleAttr(el, 'height', dimensions);
+    handleAttr(el, 'depth', dimensions);
+    handleAttr(el, 'lspace', dimensions);
+    handleAttr(el, 'voffset', dimensions);
+    return el;
+}
+
+_MathTransforms.add('mpadded', transformMpadded);
