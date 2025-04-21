72b76ecd556a9bd94abe89637f1ee7e6ff47158b
David Carlisle <d.p.carlisle@gmail.com>
2025-04-21 10:38:11 +0100

    initial code version for named space polyfill

--- namedspace/namedspace.js
+++ namedspace/namedspace.js
@@ -21,3 +21,46 @@
 */
 import { _MathTransforms } from '../common/math-transforms.js'
 
+/**
+ * @param {HTMLElement} el 
+ */
+                           // longer keynames first                           
+    const MATHML_NAMED_SPACES = {
+    veryverythinmathspace: "0.05555555555555555em",
+    verythinmathspace: "0.1111111111111111em",
+    thinmathspace: "0.16666666666666666em",
+    veryverythickmathspace: "0.3888888888888889em",
+    verythickmathspace:"0.3333333333333333em",
+    thickmathspace: "0.2777777777777778em",
+    mediummathspace: "0.2222222222222222em",
+}
+                               
+const transformNamedspace = (el) => {
+    let attr = el.getAttribute("rspace");
+    if(attr){
+    for (const [k,v] of Object.entries(MATHML_NAMED_SPACES)) {
+        attr = attr.replaceAll(k,v);
+    }
+    attr=attr.replaceAll("negative0","-0");
+    el.setAttribute("rspace", attr);
+    }
+    attr = el.getAttribute("lspace");
+    if(attr){
+    for (const [k,v] of Object.entries(MATHML_NAMED_SPACES)) {
+        attr = attr.replaceAll(k,v);
+    }
+    attr=attr.replaceAll("negative0","-0");
+    el.setAttribute("lspace", attr);
+    }
+                    //
+    return el;
+}
+
+
+// can't use comma to join selectors here it seems
+_MathTransforms.add('math *[rspace*="mathspace"]', transformNamedspace);
+_MathTransforms.add('math *[lspace*="mathspace"]', transformNamedspace);
+
+
+
+
