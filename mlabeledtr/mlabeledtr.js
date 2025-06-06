c56919b0f4a1100a3b08e097443e34acb427e6ee
NSoiffer <NSoiffer@users.noreply.github.com>
2025-06-06 13:19:25 -0700

    debug info

--- mlabeledtr/mlabeledtr.js
+++ mlabeledtr/mlabeledtr.js
@@ -64,7 +64,7 @@ function handleLabeledRows(mtable) {
       console.log('side=', side, 'length=', row.children.length);
       // leave the label as the first element or move it to the right (last element)
       for (let c = side=='left' ? 0 : 1; c < row.children.length; c++) {
-        console.log('c=', c, row.children[c].outerHTML);
+        console.log('c=', c, 'out of ', row.children.length, row.children[c].innerHTML);
         newRow.appendChild(row.children[c]);
       }
       if (side === 'right') {
