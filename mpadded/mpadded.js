4f3256d5914f05f3427fbfb2c9b48f7374281bff
NSoiffer <NSoiffer@users.noreply.github.com>
2025-06-05 21:59:40 -0700

    Add comment about what this does

--- mpadded/mpadded.js
+++ mpadded/mpadded.js
@@ -1,3 +1,6 @@
+/***
+ * Handles width/height/depth attributes with % values for mpadded
+ ***/
 // @ts-check
 /* -*- Mode: Java; tab-width: 4; indent-tabs-mode:nil; c-basic-offset: 4 -*- */
 /* vim: set ts=4 et sw=4 tw=80: */
